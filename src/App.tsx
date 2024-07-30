7a73db8156d759b079e8b38145c07194ebff39a6
Ayesha Taranum <153802441+Ayesha-Taranum@users.noreply.github.com>
2024-07-30 19:51:57 +0530

    components modified

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <>
+      <div>
+        <NavBar />
+        <TodoApp />
+      </div>
+    </>
+  )
+}
+
+export default App;
